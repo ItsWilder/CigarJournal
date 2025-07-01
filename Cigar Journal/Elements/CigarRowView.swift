ce1338f8d661211ad0974674af06728a9d96b9b1
ItsWilder <david@itswilder.com>
2025-07-01 15:12:31 -0400

    CHANGE: New view to replace struct from HomeView

--- Cigar Journal/Elements/CigarRowView.swift
+++ Cigar Journal/Elements/CigarRowView.swift
@@ -7,12 +7,67 @@
 
 import SwiftUI
 
+
+/// A view to show a single cigar in a List view
 struct CigarRowView: View {
+    let cigar: CigarTemplate
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationLink(value: cigar) {
+            HStack {
+                CigarRowImage(photo: cigar.photo)
+                    .padding(.trailing, 1)
+                
+                VStack(alignment: .leading) {
+                    Text(cigar.name)
+                        .font(.headline)
+                        .padding(.bottom, 2)
+                    Text(cigar.date.formatted(date: .numeric, time: .omitted))
+                        .font(.caption)
+                        .fontWeight(.regular)
+                        .foregroundColor(.accentColor)
+                }
+                Spacer()
+                VStack(alignment: .trailing) {
+                    Text(cigar.price == "~" ? "~" : "$\(cigar.price)")
+                        .font(.subheadline)
+                        .foregroundColor(.accentColor)
+                        .padding(.bottom, 4)
+                    StarRating(rating: cigar.rating)
+                        .font(.caption)
+                }
+            }
+        }
     }
 }
 
+private struct CigarRowImage: View {
+    var photo: Data?
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 16)
+                .fill(Color(.brown))
+                .frame(width: 60, height: 60)
+
+            if let imageData = photo,
+               let uiImage = UIImage(data: imageData) {
+                Image(uiImage: uiImage)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 60, height: 60)
+                    .clipShape(RoundedRectangle(cornerRadius: 16))
+            } else {
+                Image(systemName: "photo.on.rectangle")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 20, height: 20)
+                    .foregroundColor(Color("AccentDarkColor"))
+            }
+        }
+    }
+}
+
+
 #Preview {
-    CigarRowView()
+    CigarRowView(cigar: SampleData.cigar)
 }
