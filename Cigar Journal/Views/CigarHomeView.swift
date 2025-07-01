8d769acb3e705393e2e3cbb798486441e7429416
ItsWilder <david@itswilder.com>
2025-07-01 15:12:51 -0400

    CHANGE: Replaced struct with new view: RowView

--- Cigar Journal/Views/CigarHomeView.swift
+++ Cigar Journal/Views/CigarHomeView.swift
@@ -175,66 +175,6 @@ struct CigarListView: View {
     }
 }
 
-// Cigar Row View
-struct CigarRowView: View {
-    let cigar: CigarTemplate
-    
-    var body: some View {
-        NavigationLink(value: cigar) {
-            HStack {
-                CigarImageView(photo: cigar.photo)
-                    .padding(.trailing, 1)
-                
-                VStack(alignment: .leading) {
-                    Text(cigar.name)
-                        .font(.headline)
-                        .padding(.bottom, 2)
-                    Text(cigar.date.formatted(date: .numeric, time: .omitted))
-                        .font(.caption)
-                        .fontWeight(.regular)
-                        .foregroundColor(.accentColor)
-                }
-                Spacer()
-                VStack(alignment: .trailing) {
-                    Text(cigar.price == "~" ? "~" : "$\(cigar.price)")
-                        .font(.subheadline)
-                        .foregroundColor(.accentColor)
-                        .padding(.bottom, 4)
-                    StarRating(rating: cigar.rating)
-                        .font(.caption)
-                }
-            }
-        }
-    }
-}
-
-// Cigar Image View
-struct CigarImageView: View {
-    var photo: Data?
-    var body: some View {
-        ZStack {
-            RoundedRectangle(cornerRadius: 16)
-                .fill(Color(.brown))
-                .frame(width: 60, height: 60)
-            
-            if let imageData = photo,
-               let uiImage = UIImage(data: imageData) {
-                Image(uiImage: uiImage)
-                    .resizable()
-                    .scaledToFill()
-                    .frame(width: 60, height: 60)
-                    .clipShape(RoundedRectangle(cornerRadius: 16))
-            } else {
-                Image(systemName: "photo.on.rectangle")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 20, height: 20)
-                    .foregroundColor(Color("AccentDarkColor"))
-            }
-        }
-    }
-}
-
 // Toolbar Items
 struct ToolbarItems: ToolbarContent {
     @Binding var showAddCigar: Bool
