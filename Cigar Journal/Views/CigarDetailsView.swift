e7fb2ba737ec23e2be3df6240214cf6611fe32a4
ItsWilder <david@itswilder.com>
2025-07-01 15:52:32 -0400

    CODE: code clean up for Details View

--- Cigar Journal/Views/CigarDetailsView.swift
+++ Cigar Journal/Views/CigarDetailsView.swift
@@ -12,71 +12,64 @@ struct CigarDetails: View {
     @State private var isShowingFullImage = false
     
     var body: some View {
-        VStack {
-            TabView(selection: $selectedIndex) {
-                ForEach(cigars.indices, id: \.self) { index in
-                    ScrollView {
-                        let cigar = cigars[index]
-                        VStack(alignment: .leading) {
-                            ZStack {
-                                Rectangle().fill(Color(.brown))
-                                if let photo = cigar.photo,
-                                   let uiImage = UIImage(data: photo) {
-                                    Image(uiImage: uiImage)
-                                        .resizable()
-                                        .scaledToFill()
-                                        .frame(height: 300)
-                                        .clipped()
-                                        .onTapGesture {
-                                            isShowingFullImage = true
-                                        }
-                                } else {
-                                    Image(systemName: "photo.on.rectangle")
-                                        .resizable()
-                                        .scaledToFit()
-                                        .frame(width: 50, height: 300)
-                                        .foregroundColor(Color("AccentDarkColor"))
+        TabView(selection: $selectedIndex) {
+            ForEach(cigars.indices, id: \.self) { index in
+                ScrollView {
+                    let cigar = cigars[index]
+                    VStack(alignment: .leading) {
+                        if let photo = cigar.photo,
+                           let uiImage = UIImage(data: photo) {
+                            Image(uiImage: uiImage)
+                                .resizable()
+                                .scaledToFill()
+                                .frame(height: 300)
+                                .clipped()
+                                .onTapGesture {
+                                    isShowingFullImage = true
                                 }
+                        } else {
+                            ZStack {
+                                Rectangle()
+                                    .fill(Color(.brown))
+                                    .frame(height: 300)
+                                Image(systemName: "photo.on.rectangle")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 50)
+                                    .foregroundColor(Color("AccentDarkColor"))
                             }
-                            .padding(.top, 8)
-                            .clipShape(Rectangle())
-                            
-                            VStack(alignment: .leading) {
-                                Text(cigar.name)
-                                    .font(.largeTitle)
-                                    .fontWeight(.bold)
-                                    .foregroundColor(.accentColor)
-                                Spacer()
-                                Text(cigar.date, style: .date)
-                                    .font(.subheadline)
-                                    .opacity(0.5)
-                                Spacer()
-                                Text("\(cigar.type)  /  \(cigar.length) x \(cigar.gauge)  /  \(cigar.strength)")
-                                    .font(.title3)
-                                    .fontWeight(.semibold)
-                                    .padding(.top, 8)
-                                Spacer()
-                                Text("\(cigar.location), $\(cigar.price)")
-                                    .fontWeight(.medium)
-                                Spacer()
-                                StarRating(rating: cigar.rating)
-                                    .padding(.top, 8)
-                                Text("Notes:")
-                                    .fontWeight(.semibold)
-                                    .foregroundColor(.accentColor)
-                                    .font(.callout)
-                                    .padding(.top, 16)
-                                Spacer()
-                                Text(cigar.notes)
-                            }
-                            .padding()
                         }
+                        
+                        VStack(alignment: .leading) {
+                            Text(cigar.name)
+                                .font(.largeTitle)
+                                .fontWeight(.bold)
+                                .foregroundColor(.accentColor)
+                            Text(cigar.date, style: .date)
+                                .font(.subheadline)
+                                .opacity(0.5)
+                            Text("\(cigar.type)  /  \(cigar.length) x \(cigar.gauge)  /  \(cigar.strength)")
+                                .font(.title3)
+                                .fontWeight(.semibold)
+                                .padding(.top, 8)
+                            Text("\(cigar.location), $\(cigar.price)")
+                                .fontWeight(.medium)
+                            StarRating(rating: cigar.rating)
+                                .padding(.top, 8)
+                            Text("Notes:")
+                                .fontWeight(.semibold)
+                                .foregroundColor(.accentColor)
+                                .font(.callout)
+                                .padding(.top, 16)
+                            Text(cigar.notes)
+                        }
+                        .padding()
                     }
-                    .tag(index)
                 }
+                .tag(index)
             }
-            .tabViewStyle(.page)
         }
+        .tabViewStyle(.page)
         .toolbar {
             Button("Edit") {
                 isShowingEditSheet = true
@@ -101,7 +94,7 @@ struct CigarDetails: View {
                 } label: {
                     Image(systemName: "xmark.circle.fill")
                         .font(.system(size: 20))
-                        .foregroundColor(.gray)
+                        .foregroundColor(.white)
                         .padding()
                 }
             }
@@ -111,15 +104,5 @@ struct CigarDetails: View {
 
 
 #Preview {
-    do {
-        let config = ModelConfiguration(isStoredInMemoryOnly: true)
-        let container = try ModelContainer(for: CigarTemplate.self, configurations: config)
-        let example1 = CigarTemplate(name: "A. Flores 1975 Gran Reserva Maduro", shape: "Robusto", length: "7", gauge: "40", location: "Cigar Club", price: "100", strength: "Medium", rating: 4, notes: "Surprisingly polished despite its rough appearance. The smoke is warm and nutty, with subtle layers of honeyed tea, orange peel, and just a touch of caramel sweetness. Definitely more refined than I expected—smooth and easygoing. Would be a great choice for a relaxing evening.", date: Date())
-        let example2 = CigarTemplate(name: "Montecristo No. 2", shape: "Torpedo", length: "6 1/8", gauge: "52", location: "Local Shop", price: "15", strength: "Full", rating: 5, notes: "Classic Cuban flavor with rich earth tones and a spicy finish.", date: Date())
-        
-        return CigarDetails(cigars: [example1, example2], selectedIndex: 0)
-            .modelContainer(container)
-    } catch {
-        return Text("Failed to create preview: \(error.localizedDescription)")
-    }
+    CigarDetails(cigars: SampleData.cigars, selectedIndex: 0)
 }
